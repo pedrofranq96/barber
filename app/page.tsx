a1f8a33a46c8f3b12391f72cef9bd53ccfc86484
pedrofranq96 <pedrofranq96@gmail.com>
2024-08-13 23:28:37 -0300

    feat: ensure username

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,13 @@ const Home = async () => {
       <Header />
 
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, logado!</h2>
-        <p>Segunda-feira, 05 de agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem-vindo"}!
+        </h2>
+        <span className="capitalize">
+          {format(new Date(), "EEEE, dd", { locale: ptBR })}
+        </span>
+        <span> de {format(new Date(), "MMMM", { locale: ptBR })}.</span>
 
         <div className="mt-6">
           <Search />
